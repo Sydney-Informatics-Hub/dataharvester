774831d4bb75c22707f5249284ac0d6a57603d60
Januar Harianto <januar.harianto@sydney.edu.au>
2022-06-06 13:30:56 +1000

    feat: `.joinpath()` for os-independent file path creation

---
+++ R/joinpath.R
@@ -0,0 +1,11 @@
+#' Join path
+#'
+#' @param ...
+#' @export
+
+.joinpath <-  function(...) {
+  sep = .Platform$file.sep
+  result = gsub(paste0(sep,"{2,}"), sep, file.path(...), fixed=FALSE, perl=TRUE)
+  result = gsub(paste0(sep,"$"), '', result, fixed=FALSE, perl=TRUE)
+  return(result)
+}
