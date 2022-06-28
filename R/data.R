898f36ded7ab8fc854a2e311f36a7d164d1b6fbc
Januar Harianto <januar.harianto@sydney.edu.au>
2022-06-28 14:30:29 +1000

    docs: add documentation for example data "Llara"

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Example data Llara
+#'
+#' A dataset.
+#'
+#' @format A dataset containing x and y coordinates.
+#' \describe{
+#'   \item{Lat}{latitude}
+#'   \item{Long}{longitude}
+#'   \item{Easting}{easting}
+#'   \item{Northing}{northing}
+#'   \item{Depth}{depth as range, in cm}
+#' }
+"Llara"
