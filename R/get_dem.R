cdd063f2f791f69d8ec974392101d3f076480bd4
Januar Harianto <januar.harianto@sydney.edu.au>
2022-07-20 10:44:08 +1000

    feat: new placeholder script for getdata_dem.py #incomplete

---
+++ R/get_dem.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_dem <- function() {
+  print("in development...")
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+get_silo <- function() {
+  print("in development...")
+}
