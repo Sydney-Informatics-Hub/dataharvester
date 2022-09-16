72125862ee99026a4bc3c205860d0c1b3f68f6de
Januar Harianto <januar.harianto@sydney.edu.au>
2022-09-16 12:22:52 +1000

    feat: `download_dem()` to download from DEM

---
+++ R/dem.R
@@ -0,0 +1,25 @@
+#' Download Geoscience Australia DEM grid data
+#'
+#' Wrapper funtion to get the layers from the Geoscience Australia DEM 1 arc
+#' second grid.
+#'
+#' @param layer `string`: list of layer names to download
+#' @param out_path `string`:
+#' @param bounding_box `numeric`:
+#' @param resolution `integer`, `optional`:
+#'
+#' @return a list of output filenames
+#' @export
+#'
+#' @examples
+download_dem <- function(layer,
+                         out_path,
+                         bounding_box, zresolution = 1) {
+  # Import module
+  path <- system.file("python", package = "dataharvester")
+  dem <- reticulate::import_from_path("getdata_dem",
+    path = path,
+    delay_load = TRUE)
+  out <- dem$get_dem_layers(as.list(layer), out_path, bounding_box)
+  return(out)
+}
