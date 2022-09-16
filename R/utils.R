180ec9ebc3b7435660678b4fe03fed023b56fa37
Januar Harianto <januar.harianto@sydney.edu.au>
2022-09-16 12:06:39 +1000

    feat: `match_single()` and `match_multi()` to validate args
    This will (in the future) allow us to set defaults in ant argument for validation, e.g.:
    
    ```
    myfun <- function(x = c("a", "b", "c")) {
      match_single(x)
    }
    ```
    Then:
    ```
    myfun("a")
    #> [1] "a"
    
    myfun("x")
    #> Error: argument `x` accepts one of:  a, b, c
    ```

--- R/utils.R
+++ R/utils.R
@@ -14,3 +14,46 @@ dd_source_python <- function(modulename, rpackage, report = FALSE) {
   reticulate::import_from_path(modulename, path, convert = FALSE)
 }
 
+#' Match to one value in a function's argument
+#'
+#' @param arg
+#'
+#' @return
+#' @noRd
+match_single <- function(arg) {
+  # credit: https://stackoverflow.com/a/72438444
+  arg_name <- as.character(substitute(arg))
+  caller_fun <- sys.function(1)
+  choices_as_call <- formals(caller_fun)[[arg_name]]
+  choices <- eval(choices_as_call)
+  if (all(arg %in% choices) & length(arg) == 1) {
+    return(arg)
+  } else {
+    stop(paste0(
+      "Argument `", arg_name, "` can only contain one (1) of these ",
+      "values: ", toString(choices)
+    ))
+  }
+}
+
+#' Match to multiple values in a function's argument
+#'
+#' @param arg
+#'
+#' @return
+#' @noRd
+match_multi <- function(arg) {
+  # credit: https://stackoverflow.com/a/72438444
+  arg_name <- as.character(substitute(arg))
+  caller_fun <- sys.function(1)
+  choices_as_call <- formals(caller_fun)[[arg_name]]
+  choices <- eval(choices_as_call)
+  if (all(arg %in% choices)) {
+    return(arg)
+  } else {
+    stop(paste0(
+      "Argument `", arg_name, "` can only contain these values: ",
+      toString(choices)
+    ))
+  }
+}
