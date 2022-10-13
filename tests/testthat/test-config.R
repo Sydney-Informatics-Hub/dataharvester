cdf12a98ff7fe78a05a22154a9e1aedcd703b234
Januar Harianto <januar.harianto@sydney.edu.au>
2022-10-14 10:07:46 +1100

    test: update tests

--- tests/testthat/test-config.R
+++
@@ -1,6 +0,0 @@
-# load_config() -----
-test_that("load_config() uses a default config when no argument is provided", {
-  test_config <- default_config
-  class(test_config) <- append(class(test_config), "harvester")
-  expect_equal(load_config(), test_config)
-})
---
+++ tests/testthat/test-dea.R
@@ -0,0 +1,31 @@
+# generate temp directory
+
+
+test_that("download_dea: downloads work", {
+  skip_on_cran()
+  skip_if_no_conda()
+
+  llara <- c(149.7, -30.3, 149.8, -30.2)
+  tempDir <- tempfile()
+  dir.create(tempDir)
+
+  dea1 <- download_dea(
+    layer = "landsat_barest_earth",
+    bounding_box = llara,
+    out_path = tempDir,
+    years = 2021,
+    resolution = 300
+  )
+  unlink(dea1)
+  dea2 <- download_dea(
+    layer = c("landsat_barest_earth", "ga_ls_fc_pc_cyear_3"),
+    bounding_box = llara,
+    out_path = tempDir,
+    years = 2021,
+    resolution = 300
+  )
+  expect_true("rasterPath" %in% class(dea2))
+  expect_equal(length(dea2), 2)
+  unlink(tempDir, recursive = TRUE)
+})
+
